dc39231a241d299880f5c232399cbd4011b958d2
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2021-05-13 19:44:27 -0400

    feat: init

---
+++ socketio-node/index.js
@@ -0,0 +1,43 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origins: ['http://localhost:3001', 'http://localhost:4200']
+  }
+});
+
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  console.log('token', token);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hey Socket.io</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('my message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('my broadcast', `server received: ${msg}`);
+  });
+
+  socket.on('marco', (msg) => {
+    console.log('marco');
+    io.emit('marco', `polo`);
+  });
+
+  setInterval(() => {
+    io.emit('ping', { type: 'ping', payload: Math.random().toString() });
+  }, 10000);
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
